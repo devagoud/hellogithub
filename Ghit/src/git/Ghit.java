663e01e3fdc61f38b43150a60370de5153b9aba1
devagoud <31921521+devagoud@users.noreply.github.com>
2018-01-20 18:01:11 +0530

    commeted hit project

---
+++ Ghit/src/git/Ghit.java
@@ -0,0 +1,9 @@
+package git;
+
+public class Ghit {
+
+	public static void main(String []args) {
+		System.out.println("haiii git hub");
+		
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# hellogithub
+how to use hit hub
